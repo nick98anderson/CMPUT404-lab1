770a32f3e032ae5901e05cdf19e1999d7228d07f
Nick Anderson <47461646+nick98anderson@users.noreply.github.com>
2022-01-11 20:57:24 -0600

    get request

--- scripts.py
+++ scripts.py
@@ -1,7 +1,9 @@
 import requests
 
-print(requests.__version__)
+print("requestons version: ", requests.__version__)
 
 r = requests.get("http://www.google.com")
 
-print(r.status_code)
+raw_url = 'https://raw.githubusercontent.com/nick98anderson/CMPUT404-lab1/master/scripts.py?token=GHSAT0AAAAAABQL736YMASG35U6BV6VXZWYYO6IMQA'
+script = requests.get(raw_url)
+print(script.content.decode())
